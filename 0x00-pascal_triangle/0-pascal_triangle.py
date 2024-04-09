173564112da1044d6f7136603898ffad4bc278f7
eliasyeme <eliasyeme@gmail.com>
2024-04-09 04:44:07 +0300

    Pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,13 @@
+def pascal_triangle(n):
+    result = [[1]]
+
+    if n <= 0:
+        return []
+
+    for i in range(n - 1):
+        last_row = [0] + result[-1] + [0]
+        current_row = []
+        for j in range(len(result[-1]) + 1):
+            current_row.append(last_row[j] + last_row[j + 1])
+        result.append(current_row)
+    return result
